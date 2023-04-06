b8270ef50fdfcdf4b462c5f1ba5ddc16dbbcb715
Mateusz Świnairski <eirdom@gmail.com>
2023-04-06 20:06:19 +0200

    first commit

---
+++ src/main/java/pl/rodzyn/lotto/Game.java
@@ -0,0 +1,6 @@
+package pl.rodzyn.lotto;
+
+public interface Game {
+
+    void start();
+}
---
+++ src/main/java/pl/rodzyn/lotto/Lotto.java
@@ -0,0 +1,7 @@
+package pl.rodzyn.lotto;
+
+public class Lotto implements Game {
+    public void start() {
+
+    }
+}
